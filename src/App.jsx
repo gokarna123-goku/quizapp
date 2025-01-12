616bf537e98c3902e17e0619938ad0b2cab21b63
Gokarna Adhikari <agokarna444@gmail.com>
2025-01-12 21:01:44 +0545

    first initialize the quiz app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <main className="w-full h-screen bg-neutral-50 text-neutral-600 flex flex-col">
+        This is the main section.
+      </main>
+    </>
+  )
+}
+
+export default App
